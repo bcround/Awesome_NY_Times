4b3ddabc683fc744c9a6ec62848ec976d0947729
bcround <sbc1142@gmail.com>
2021-07-02 14:09:29 +0900

    Feat: Set global style modified:   src/styles/global-style.ts

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -1,10 +1,25 @@
-import { create } from 'domain';
 import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
   /* Add global style below if you need */
+  html {
+    font-size: 10px;
+    background-color: #f8f9fa;
+  }
+  body,
+  body *,
+  body::before,
+  body::after,
+  body *::before,
+  body *::after {
+    box-sizing: border-box;
+  }
+  button {
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyle;
