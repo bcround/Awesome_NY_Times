b078c74d2e97d9ef3b7a0450f13fd8ea8bd7f020
bcround <sbc1142@gmail.com>
2021-07-04 01:20:21 +0900

    Feat: Create page not found page modified:   src/pages/PageNotFound.tsx

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,13 @@
+import styled from 'styled-components';
+
+const NotFoundParagraph = styled.p`
+  text-align: center;
+  margin-top: 20%;
+  font-size: 3rem;
+`;
+
 const PageNotFound = () => {
-  return <p>Page Not Found</p>;
+  return <NotFoundParagraph>Requested Page is not found!</NotFoundParagraph>;
 };
 
 export default PageNotFound;
