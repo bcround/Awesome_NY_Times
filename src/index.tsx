0ef6be3783db80b6642ed9af97bb71d85dfcd8f9
bcround <sbc1142@gmail.com>
2021-07-04 01:09:53 +0900

    Feat: To store data locally, use redux-persist modified:   package-lock.json modified:   package.json modified:   src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import App from '@/app/App';
 import { GlobalStyle } from '@/styles/global-style';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,16 +14,28 @@ import { ScrollToTop } from '@/components';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['article', 'favorite'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
     <Provider store={store}>
-      <Router>
-        <ScrollToTop />
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <ScrollToTop />
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
