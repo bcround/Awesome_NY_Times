3246a599d8c8ffdd2a4fda622049bc65b91e996e
bcround <sbc1142@gmail.com>
2021-07-04 01:12:07 +0900

    Feat: Remove unnecessary import statement modified:   src/styles/global-style.ts

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle, css } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 import { reset } from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
