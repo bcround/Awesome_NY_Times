7864ecefe1e84203965a7d168e2dc3be571986c1
bcround <sbc1142@gmail.com>
2021-07-03 16:20:08 +0900

    Feat: Set up routers modified:   package-lock.json modified:   package.json modified:   src/app/App.tsx modified:   src/index.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { ArticleSearch } from '@/pages';
+import { ArticleSearch, PageNotFound } from '@/pages';
+import { Header } from '@/components';
 import styled from 'styled-components';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 const AppMain = styled.main``;
 
 const App: React.FC = () => {
   return (
-    <AppMain>
-      <ArticleSearch />
-    </AppMain>
+    <>
+      <Header />
+      <AppMain>
+        <Switch>
+          <Route path="/" exact component={ArticleSearch} />
+          <Route path="/page-not-found" component={PageNotFound} />
+          <Redirect to="/page-not-found" />
+        </Switch>
+      </AppMain>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/app/App';
-import GlobalStyle from '@/styles/global-style';
+import { GlobalStyle } from '@/styles/global-style';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ScrollToTop } from '@/components';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules';
 
@@ -15,7 +17,10 @@ ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
     <Provider store={store}>
-      <App />
+      <Router>
+        <ScrollToTop />
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
